393cafaa1560a83e230df3b4b17782a22bf3a99c
Rolf Schmidt <rolf.schmidt@zammad.com>
2020-12-01 12:54:38 +0100

    use early return

--- day01.v
+++ day01.v
@@ -2,46 +2,30 @@ module main
 
 fn day01a() int {
     numbers := read_day('day01.txt').map(it.int())
-    mut result := 0
 
     for number1 in numbers {
         for number2 in numbers {
             if number1 + number2 == 2020 {
-                result = number1 * number2
-                break
+                return number1 * number2
             }
-        }
-
-        if result > 0 {
-            break
         }
     }
 
-    return result
+    return 0
 }
 
 fn day01b() int {
     numbers := read_day('day01.txt').map(it.int())
-    mut result := 0
 
     for number1 in numbers {
         for number2 in numbers {
             for number3 in numbers {
                 if number1 + number2 + number3 == 2020 {
-                    result = number1 * number2 * number3
-                    break
+                    return number1 * number2 * number3
                 }
             }
-
-            if result > 0 {
-                break
-            }
-        }
-
-        if result > 0 {
-            break
         }
     }
 
-    return result
+    return 0
 }
