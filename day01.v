dd18ed31e736d8d489747ff5d4d484339c4bfd62
Rolf Schmidt <rolf.schmidt@zammad.com>
2020-12-01 12:51:32 +0100

    cast array to int before running checks

--- day01.v
+++ day01.v
@@ -1,13 +1,13 @@
 module main
 
 fn day01a() int {
-    numbers := read_day('day01.txt')
+    numbers := read_day('day01.txt').map(it.int())
     mut result := 0
 
     for number1 in numbers {
         for number2 in numbers {
-            if ( number1.int() + number2.int() ) == 2020 {
-                result = number1.int() * number2.int()
+            if ( number1 + number2 ) == 2020 {
+                result = number1 * number2
                 break
             }
         }
@@ -21,14 +21,14 @@ fn day01a() int {
 }
 
 fn day01b() int {
-    numbers := read_day('day01.txt')
+    numbers := read_day('day01.txt').map(it.int())
     mut result := 0
 
     for number1 in numbers {
         for number2 in numbers {
             for number3 in numbers {
-                if ( number1.int() + number2.int() + number3.int() ) == 2020 {
-                    result = number1.int() * number2.int() * number3.int()
+                if ( number1 + number2 + number3 ) == 2020 {
+                    result = number1 * number2 * number3
                     break
                 }
             }
