f056da4f513fc9d9ac5a4ae1f831b614ec66505e
Rolf Schmidt <rolf.schmidt@zammad.com>
2020-12-05 11:45:25 +0100

    fix func name

--- day05.v
+++ day05.v
@@ -1,6 +1,6 @@
 module main
 
-fn partionate(code string, range_low_init int, range_high_init int, index int) int {
+fn d5_partionate(code string, range_low_init int, range_high_init int, index int) int {
 	mut range_low := range_low_init
 	mut range_high := range_high_init
 	if index > code.len - 1 {
@@ -16,14 +16,14 @@ fn partionate(code string, range_low_init int, range_high_init int, index int) i
 	} else if typ == `B` || typ == `R` {
 		range_low = (range_high + 1 - range_low) / 2 + range_low
 	}
-	return partionate(code, range_low, range_high, index + 1)
+	return d5_partionate(code, range_low, range_high, index + 1)
 }
 
 fn d5_seats(seat_data []string) []int {
 	mut result := []int{}
 	for seat_string in seat_data {
-		column := partionate(seat_string[0..seat_string.len - 3], 0, 127, 0)
-		row := partionate(seat_string[seat_string.len - 3..seat_string.len], 0, 7, 0)
+		column := d5_partionate(seat_string[0..seat_string.len - 3], 0, 127, 0)
+		row := d5_partionate(seat_string[seat_string.len - 3..seat_string.len], 0, 7, 0)
 		result << column * 8 + row
 	}
     result.sort()
