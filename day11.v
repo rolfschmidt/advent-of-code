09d5a8f0e4d03f48bdc5d02f78e5530a593226de
Rolf Schmidt <rolf.schmidt@zammad.com>
2020-12-11 19:12:59 +0100

    double speed d11

--- day11.v
+++ day11.v
@@ -1,26 +1,13 @@
 module main
 
-fn d11_seat_pos(lines [][]string, x int, y int) bool {
-	if !astr_valid_index(lines[0], x) || !aastr_valid_index(lines, y) {
-		return false
-	}
-	return lines[y][x] == 'L'
-}
-
-fn d11_occupied_pos(lines [][]string, x int, y int) bool {
-	if !astr_valid_index(lines[0], x) || !aastr_valid_index(lines, y) {
-		return false
-	}
-	return lines[y][x] == '#'
-}
-
 fn d11_toggle_occupied(lines [][]string, occupied_range int, x int, y int) bool {
 	mut count := 0
-	mut ranges := [][][]int{}
 	mut minx := 0
 	mut miny := 0
 	mut maxx := lines[x].len
 	mut maxy := lines.len
+	lastx := lines[x].len - 1
+	lasty := lines.len - 1
 	if occupied_range != 5 {
 		minx = x - 1
 		miny = y - 1
@@ -28,33 +15,147 @@ fn d11_toggle_occupied(lines [][]string, occupied_range int, x int, y int) bool 
 		maxy = y + 1
 	}
 	// left
-	ranges << aint_range(x - 1, minx).map([it, y])
+	for pos in aint_range(x - 1, minx) {
+		posx := pos
+		posy := y
+		if posx < 0 || posx > lastx || posy < 0 || posy > lasty {
+			continue
+		}
+		if posx == x && posy == y {
+			continue
+		}
+		if lines[posy][posx] == 'L' {
+			break
+		}
+		if lines[posy][posx] == '#' {
+			count++
+			break
+		}
+	}
 	// right
-	ranges << aint_range(x + 1, maxx).map([it, y])
+	for pos in aint_range(x + 1, maxx) {
+		posx := pos
+		posy := y
+		if posx < 0 || posx > lastx || posy < 0 || posy > lasty {
+			continue
+		}
+		if posx == x && posy == y {
+			continue
+		}
+		if lines[posy][posx] == 'L' {
+			break
+		}
+		if lines[posy][posx] == '#' {
+			count++
+			break
+		}
+	}
 	// top
-	ranges << aint_range(y - 1, miny).map([x, it])
+	for pos in aint_range(y - 1, miny) {
+		posx := x
+		posy := pos
+		if posx < 0 || posx > lastx || posy < 0 || posy > lasty {
+			continue
+		}
+		if posx == x && posy == y {
+			continue
+		}
+		if lines[posy][posx] == 'L' {
+			break
+		}
+		if lines[posy][posx] == '#' {
+			count++
+			break
+		}
+	}
 	// bottom
-	ranges << aint_range(y + 1, maxy).map([x, it])
+	for pos in aint_range(y + 1, maxy) {
+		posx := x
+		posy := pos
+		if posx < 0 || posx > lastx || posy < 0 || posy > lasty {
+			continue
+		}
+		if posx == x && posy == y {
+			continue
+		}
+		if lines[posy][posx] == 'L' {
+			break
+		}
+		if lines[posy][posx] == '#' {
+			count++
+			break
+		}
+	}
 	// left top
-	ranges << aint_diagonal_range(x - 1, y - 1, minx, miny)
+	for pos in aint_diagonal_range(x - 1, y - 1, minx, miny) {
+		posx := pos[0]
+		posy := pos[1]
+		if posx < 0 || posx > lastx || posy < 0 || posy > lasty {
+			continue
+		}
+		if posx == x && posy == y {
+			continue
+		}
+		if lines[posy][posx] == 'L' {
+			break
+		}
+		if lines[posy][posx] == '#' {
+			count++
+			break
+		}
+	}
 	// right top
-	ranges << aint_diagonal_range(x + 1, y - 1, maxx, miny)
+	for pos in aint_diagonal_range(x + 1, y - 1, maxx, miny) {
+		posx := pos[0]
+		posy := pos[1]
+		if posx < 0 || posx > lastx || posy < 0 || posy > lasty {
+			continue
+		}
+		if posx == x && posy == y {
+			continue
+		}
+		if lines[posy][posx] == 'L' {
+			break
+		}
+		if lines[posy][posx] == '#' {
+			count++
+			break
+		}
+	}
 	// left bottom
-	ranges << aint_diagonal_range(x - 1, y + 1, 0, maxy)
+	for pos in aint_diagonal_range(x - 1, y + 1, 0, maxy) {
+		posx := pos[0]
+		posy := pos[1]
+		if posx < 0 || posx > lastx || posy < 0 || posy > lasty {
+			continue
+		}
+		if posx == x && posy == y {
+			continue
+		}
+		if lines[posy][posx] == 'L' {
+			break
+		}
+		if lines[posy][posx] == '#' {
+			count++
+			break
+		}
+	}
 	// right bottom
-	ranges << aint_diagonal_range(x + 1, y + 1, maxx, maxy)
-	for rr in ranges {
-		for pos in rr {
-			if pos[0] == x && pos[1] == y {
-				continue
-			}
-			if d11_seat_pos(lines, pos[0], pos[1]) {
-				break
-			}
-			if d11_occupied_pos(lines, pos[0], pos[1]) {
-				count++
-				break
-			}
+	for pos in aint_diagonal_range(x + 1, y + 1, maxx, maxy) {
+		posx := pos[0]
+		posy := pos[1]
+		if posx < 0 || posx > lastx || posy < 0 || posy > lasty {
+			continue
+		}
+		if posx == x && posy == y {
+			continue
+		}
+		if lines[posy][posx] == 'L' {
+			break
+		}
+		if lines[posy][posx] == '#' {
+			count++
+			break
 		}
 	}
 	return (lines[y][x] == 'L' && count == 0) || (lines[y][x] == '#' && count >= occupied_range)
