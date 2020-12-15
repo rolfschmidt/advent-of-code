2a917f6b9391ef6501580068bcc35d5cf13570d4
Rolf Schmidt <rolf.schmidt@zammad.com>
2020-12-15 17:41:47 +0100

    little performance boost

--- day15.v
+++ day15.v
@@ -5,34 +5,34 @@ fn d15_run(lines string, find int) int {
 	mut spoken_before := map[string]int{}
 	mut spoken := map[string]int{}
 	mut numbers := lines.split(',')
-	for i := 0; i < numbers.len; i++ {
-		number_str := numbers[i]
-		if i == numbers.len - 1 {
-			if !spoken.exists(number_str) {
-				numbers << '0'
-				spoken[number_str] = i
-			} else {
-				spoken_before[number_str] = spoken[number_str]
-				spoken[number_str] = i
-				last_index := spoken[number_str]
-				mut last_index2 := 0
-				if spoken_before.exists(number_str) {
-					last_index2 = spoken_before[number_str]
-				}
-				if last_index == i && last_index2 == i - 1 {
-					numbers << '1'
-				} else {
-					numbers << ((last_index + 1) - (last_index2 + 1)).str()
-				}
-			}
-		} else {
+	for i, number in numbers {
+		spoken[number.str()] = i
+	}
+	mut last_number := numbers.last()
+	mut last_index := 0
+	mut last_index2 := 0
+	mut number_str := ''
+	for i := numbers.len - 1; i < find - 1; i++ {
+		number_str = last_number
+		if !spoken.exists(number_str) {
+			last_number = '0'
 			spoken[number_str] = i
-		}
-		if i == find - 2 {
-			return numbers.last().int()
+		} else {
+			spoken_before[number_str] = spoken[number_str]
+			spoken[number_str] = i
+			last_index = spoken[number_str]
+			last_index2 = 0
+			if spoken_before.exists(number_str) {
+				last_index2 = spoken_before[number_str]
+			}
+			if last_index == i && last_index2 == i - 1 {
+				last_number = '1'
+			} else {
+				last_number = ((last_index + 1) - (last_index2 + 1)).str()
+			}
 		}
 	}
-	return 0
+	return last_number.int()
 }
 
 fn day15a() int {
