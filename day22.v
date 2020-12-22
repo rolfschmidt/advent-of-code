c60d892e37ae70fb1105f39de79f554fb05100d4
Rolf Schmidt <rolf@schmidt.ink>
2020-12-22 19:06:58 +0100

    added day 22

--- day22.v
+++ day22.v
@@ -1,69 +1,68 @@
 module main
 
-struct Card {
-pub mut:
-    value int
-    score int
-}
-
-fn (c Card) clone() Card {
-    return Card{ value: c.value, score: c.score }
-}
-
-struct Player {
-pub mut:
-    cards []Card
-    score int
+[direct_array_access]
+fn d22_game(pp1 []int, pp2 []int, part2 bool) ([]int, []int) {
+	mut p1 := pp1.clone()
+	mut p2 := pp2.clone()
+	mut seen := map[string]bool{}
+	for p1.len > 0 && p2.len > 0 {
+		mut p1_wins := true
+		if seen['$p1;$p2'] && part2 {
+			break
+		}
+		seen['$p1;$p2'] = true
+		p1c := p1[0]
+		p2c := p2[0]
+		p1.delete(0)
+		p2.delete(0)
+		if p1c <= p1.len && p2c <= p2.len && part2 {
+			pm1, _ := d22_game(p1[0..p1c], p2[0..p2c], part2)
+			if pm1.len > 0 {
+				p1_wins = true
+			} else {
+				p1_wins = false
+			}
+		} else {
+			if p1c > p2c {
+				p1_wins = true
+			} else {
+				p1_wins = false
+			}
+		}
+		if p1_wins {
+			p1 << p1c
+			p1 << p2c
+		} else {
+			p2 << p2c
+			p2 << p1c
+		}
+	}
+	return p1, p2
 }
 
 fn d22_run(part2 bool) int {
 	mut lines := read_day_string('day22.txt')
-    blocks := lines.split('\n\n')
-
-    mut players := []Player{ len: 2, init: Player{} }
-    for card in blocks[0].all_after(':\n').split('\n').map(it.int()) {
-        players[0].cards << Card{ value: card }
-    }
-    for card in blocks[1].all_after(':\n').split('\n').map(it.int()) {
-        players[1].cards << Card{ value: card }
-    }
-
-    for mut pi1, p1 in players {
-        for mut pi2, p2 in players {
-            if pi1 == pi2 {
-                continue
-            }
-
-            for p1.cards.len > 0 && p2.cards.len > 0 {
-                println(p1.cards.map(it.value))
-                println(p2.cards.map(it.value))
-                if p1.cards[0].value > p2.cards[0].value {
-                    p1.cards << p1.cards[0]
-                    p1.cards << p2.cards[0]
-                    p1.cards.delete(0)
-                    p2.cards.delete(0)
-                }
-                else {
-                    p2.cards << p2.cards[0]
-                    p2.cards << p1.cards[0]
-                    p1.cards.delete(0)
-                    p2.cards.delete(0)
-                }
-            }
-
-            break
-        }
-    }
-
-    for mut player in players {
-        player.cards.reverse_in_place()
-        for mut i, card in player.cards {
-            card.score = (i + 1) * card.value
-        }
-        player.cards.reverse_in_place()
-    }
-
-	return int_max(aint_sum(players[0].cards.map(it.score)), aint_sum(players[1].cards.map(it.score)))
+	blocks := lines.split('\n\n')
+	mut players := [][]int{}
+	players << []int{}
+	players << []int{}
+	for card in blocks[0].all_after(':\n').split('\n').map(it.int()) {
+		players[0] << card
+	}
+	for card in blocks[1].all_after(':\n').split('\n').map(it.int()) {
+		players[1] << card
+	}
+	pm1, pm2 := d22_game(players[0], players[1], part2)
+	players[0] = pm1
+	players[1] = pm2
+	for pi, player in players {
+		players[pi].reverse_in_place()
+		for i, card in player {
+			players[pi][i] = (i + 1) * card
+		}
+		players[pi].reverse_in_place()
+	}
+	return int_max(aint_sum(players[0].map(it)), aint_sum(players[1].map(it)))
 }
 
 fn day22a() int {
@@ -71,5 +70,5 @@ fn day22a() int {
 }
 
 fn day22b() int {
-	return 0
+	return d22_run(true)
 }
