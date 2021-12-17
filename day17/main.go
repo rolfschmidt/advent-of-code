0e05a313b9fac32cfe610fdd91cabdad1dd30b63
Rolf Schmidt <rolf.schmidt@zammad.com>
2021-12-17 08:00:56 +0000

    add day 17

---
+++ day17/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+    "../helper"
+)
+
+func main() {
+    fmt.Println("Part 1", Part1())
+    fmt.Println("Part 2", Part2())
+}
+
+func Part1() int {
+    return Run(false)
+}
+
+func Part2() int {
+    return Run(true)
+}
+
+func Run(Part2 bool) int {
+
+    // target area: x=20..30, y=-10..-5
+    input := helper.ReadFileString("input.txt")
+
+    xs := helper.String2Int(input[strings.IndexByte(input, '=')+1:strings.IndexByte(input, '.')])
+    input = input[strings.IndexByte(input, '.')+2:]
+
+    xe := helper.String2Int(input[:strings.IndexByte(input, ',')])
+    input = input[strings.IndexByte(input, ',')+4:]
+
+    ys := helper.String2Int(input[:strings.IndexByte(input, '.')])
+    input = input[strings.IndexByte(input, '.')+2:]
+
+    ye := helper.String2Int(input)
+
+    matrix := map[int]map[int]int{}
+    for y := ys; y <= ye; y++ {
+        for x := xs; x <= xe; x++ {
+            if _, ok := matrix[y]; !ok {
+                matrix[y] = map[int]int{}
+            }
+
+            matrix[y][x] = 1
+        }
+    }
+
+    result := 0
+    for vx := 0; vx < 1000; vx += 1 {
+        for vy := -1000; vy < 1000; vy += 1 {
+            px := 0
+            py := 0
+            rx := vx
+            ry := vy
+            hp := 0
+            for px < xe && py > ys {
+                px += rx
+                py += ry
+                hp = helper.IntMax(hp, py)
+
+                if _, ok := matrix[py]; !ok {
+                    matrix[py] = map[int]int{}
+                }
+
+                if matrix[py][px] == 1 {
+                    if !Part2 {
+                        result = helper.IntMax(hp, result)
+                    } else {
+                        result += 1
+                    }
+                    break
+                }
+
+                if rx > 0 {
+                    rx -= 1
+                }
+                ry -= 1
+            }
+        }
+    }
+
+    // for y := 15; y > -12; y-- {
+    //     for x := 0; x < 32; x++ {
+    //         if x == 0 && y == 0 {
+    //             fmt.Print("S")
+    //         } else if v, ok := matrix[y][x]; ok {
+    //             fmt.Print(v)
+    //         } else {
+    //             fmt.Print(".")
+    //         }
+    //     }
+    //     fmt.Println()
+    // }
+
+    return result
+}
