32552a77b74dedd4cc7338abc1d319dfd92ab489
Rolf Schmidt <rolf.schmidt@zammad.com>
2022-12-03 05:51:43 +0000

    add day 3

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,5 +1,15 @@
+class String
+  def halve
+    [self[0, self.size/2], self[self.size/2..-1]]
+  end
+end
+
 class Helper
   def self.file_string
     File.read("spec/#{self.to_s.downcase}.txt")
+  end
+
+  def self.all_chars
+    ("a".."z").to_a + ("A".."Z").to_a
   end
 end
