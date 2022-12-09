5c75e843b02e8057d7b023a4015f4e3c4dc8cc08
Rolf Schmidt <rolf.schmidt@zammad.com>
2022-12-09 08:57:03 +0000

    improve codestyle

--- spec/day09_spec.rb
+++ spec/day09_spec.rb
@@ -3,42 +3,39 @@ class Day09 < Helper
   def self.part1(chain_count = 2)
     chains  = chain_count.times.map { Vector[0, 0] }
     checked = (0..chains.count - 1).map {|i| [i,[Vector[0, 0]]] }.to_h
+    move    = {
+      "R" => Vector[1, 0],
+      "L" => Vector[-1, 0],
+      "U" => Vector[0, 1],
+      "D" => Vector[0, -1]
+    }
 
     file.split("\n").each_with_index do |line, i|
       d, c = line.split(" ")
 
       c.to_i.times do |t|
-        if d == "R"
-          chains[0][0] += 1
-        elsif d == "L"
-          chains[0][0] -= 1
-        elsif d == "U"
-          chains[0][1] += 1
-        elsif d == "D"
-          chains[0][1] -= 1
-        end
-
+        chains[0] += move[d]
         checked[0].unshift(chains[0].clone)
 
         (1..chains.size - 1).each do |i|
           ca = chains[i - 1]
           cb = chains[i]
 
-          if (ca - cb).magnitude >= 2
-            if ca[0] > cb[0]
-              cb[0] += 1
-            elsif ca[0] < cb[0]
-              cb[0] -= 1
-            end
+          next if (ca - cb).magnitude < 2
 
-            if ca[1] > cb[1]
-              cb[1] += 1
-            elsif ca[1] < cb[1]
-              cb[1] -= 1
-            end
-
-            checked[i].unshift(cb.clone)
+          if ca[0] > cb[0]
+            cb[0] += 1
+          elsif ca[0] < cb[0]
+            cb[0] -= 1
           end
+
+          if ca[1] > cb[1]
+            cb[1] += 1
+          elsif ca[1] < cb[1]
+            cb[1] -= 1
+          end
+
+          checked[i].unshift(cb.clone)
         end
       end
     end
