22c2a08592faa52940daf9d7568dcef54101403c
Rolf Schmidt <rolf.schmidt@zammad.com>
2022-12-23 06:12:00 +0000

    add day 23 part 1

---
+++ 2022/spec/day23_spec.rb
@@ -0,0 +1,102 @@
+class Day23 < Helper
+  def self.part1(part2 = false)
+    grid = {}
+    file_test.split("\n").map{|v| v.split("") }.each_with_index do |yv, y|
+      yv.each_with_index do |xv, x|
+        next if xv != "#"
+
+        grid[Vector[x, y]] = true
+      end
+    end
+
+    old_grid    = ddup(grid)
+    roles_index = 0
+    rules       = [:north, :south, :west, :east]
+    moved       = false
+    round       = 0
+
+    (part2 ? 1e12 : 10).to_i.times do
+      round += 1
+      moves = {}
+
+      if round % 100 == 0
+        puts round
+      end
+
+      old_grid.each do |pos, _|
+        north = old_grid[pos + Vector[-1, -1]] || old_grid[pos + Vector[0, -1]] || old_grid[pos + Vector[1, -1]]
+        south = old_grid[pos + Vector[-1, 1]] || old_grid[pos + Vector[0, 1]] || old_grid[pos + Vector[1, 1]]
+        west  = old_grid[pos + Vector[-1, -1]] || old_grid[pos + Vector[-1, 0]] || old_grid[pos + Vector[-1, 1]]
+        east  = old_grid[pos + Vector[1, -1]] || old_grid[pos + Vector[1, 0]] || old_grid[pos + Vector[1, 1]]
+
+        next if !north && !south && !west && !east
+
+        rules.each do |rule|
+          if !north && rule == :north
+            moves[pos + Vector[0, -1]] ||= []
+            moves[pos + Vector[0, -1]] << pos
+          elsif !south && rule == :south
+            moves[pos + Vector[0, 1]] ||= []
+            moves[pos + Vector[0, 1]] << pos
+          elsif !west && rule == :west
+            moves[pos + Vector[-1, 0]] ||= []
+            moves[pos + Vector[-1, 0]] << pos
+          elsif !east && rule == :east
+            moves[pos + Vector[1, 0]] ||= []
+            moves[pos + Vector[1, 0]] << pos
+          else
+            next
+          end
+          break
+        end
+      end
+
+      moves.each do |to, froms|
+        next if moves[to].count > 1
+
+        pos = froms.shift
+        grid.delete(pos)
+        grid[to] = true
+      end
+
+      # roles_index = (rules_index + 1) % rules.size
+      rules << rules.shift
+      old_grid = ddup(grid)
+
+      break if moves.blank? && part2
+    end
+
+    minx = grid.map{|v, _| v[0] }.min
+    maxx = grid.map{|v, _| v[0] }.max
+    miny = grid.map{|v, _| v[1] }.min
+    maxy = grid.map{|v, _| v[1] }.max
+
+    free = {}
+    counter = 0
+    (miny..maxy).each do |y|
+      (minx..maxx).each do |x|
+        next if grid[Vector[x, y]]
+
+        counter += 1
+      end
+    end
+
+    return round if part2
+
+    counter
+  end
+
+  def self.part2
+    part1(true)
+  end
+end
+
+RSpec.describe "Day23" do
+  it "does part 1" do
+    expect(Day23.part1).to eq(4241)
+  end
+
+  # it "does part 2" do
+  #   expect(Day23.part2).to eq(100)
+  # end
+end
