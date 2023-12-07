62d60b964bf36173c8a867045747218fff0bbc5d
Rolf Schmidt <rolf.schmidt@zammad.com>
2023-12-07 08:11:09 +0000

    use case stmt

--- 2023/spec/day07_spec.rb
+++ 2023/spec/day07_spec.rb
@@ -1,8 +1,8 @@
 class Day07 < Helper
   def self.cards_points(cards, part2)
     cards_replaced = ddup(cards)
-    types = cards.tally.invert
-    two_pairs = cards.select{|c| cards.select{|cs| c == cs }.count == 2 }.uniq
+    types          = cards.tally.invert
+    two_pairs      = cards.select{|c| cards.select{|cs| c == cs }.count == 2 }.uniq
 
     if part2 && cards.include?(1)
       types = cards.select{|c| c != 1 }.tally.invert
@@ -18,12 +18,10 @@ class Day07 < Helper
                   end
 
         cards_replaced = cards_replaced.map do |c|
-          if c == 1 && jokers > 0
-            jokers -= 1
-            replace
-          else
-            c
-          end
+          next c if c != 1 || jokers == 0
+
+          jokers -= 1
+          replace
         end
 
         break
@@ -55,38 +53,26 @@ class Day07 < Helper
   def self.part1(part2 = false)
     players = file.split("\n").map do |line|
       data = line.split(" ")
-      {
-        cards: data[0].chars.map {|c|
-          if c =~ /\d/
-            c.to_i
-          elsif c == 'T'
-            10
-          elsif c == 'J'
-            if part2
-              1
-            else
-              11
-            end
-          elsif c == 'Q'
-            12
-          elsif c == 'K'
-            13
-          elsif c == 'A'
-            14
-          end
-        },
-        points: data[1].to_i,
-      }
-    end
-
-    result = players.sort do |a, b|
-      cards_points(a[:cards], part2) <=> cards_points(b[:cards], part2)
+      data[0] = data[0].chars.map do |c|
+        case c
+          when /\d/ then c.to_i
+          when 'T' then 10
+          when 'J' then (part2 ? 1 : 11)
+          when 'Q' then 12
+          when 'K' then 13
+          when 'A' then 14
+        end
+      end
+      data[1] = data[1].to_i
+      data
     end
 
     count = 0
-    result.sum do |player|
+    players.sort do |a, b|
+      cards_points(a[0], part2) <=> cards_points(b[0], part2)
+    end.sum do |player|
       count += 1
-      count * player[:points]
+      count * player[1]
     end
   end
 
