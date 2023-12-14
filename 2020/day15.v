9f157a262ba1f21ba81b4669d2735a8f20e0fafe
Rolf Schmidt <rolf@schmidt.ink>
2023-12-14 17:53:13 +0100

    optimize 2020/15

--- 2020/day15.v
+++ 2020/day15.v
@@ -2,34 +2,29 @@ module main
 
 [inline]
 fn d15_run(lines string, find int) int {
-	mut spoken_before := map[string]int{}
-	mut spoken := map[string]int{}
-	mut numbers := lines.split(',')
-	for i, number in numbers {
-		spoken[number.str()] = i
-	}
-	mut current_number := ''
+	mut numbers := lines.split(',').map(it.int())
 	mut last_number := numbers.last()
-	mut last_index := 0
-	mut last_index2 := 0
-	for i := numbers.len - 1; i < find - 1; i++ {
-		current_number = last_number
-		if current_number !in spoken {
-			last_number = '0'
-			spoken[current_number] = i
-		} else {
-			spoken_before[current_number] = spoken[current_number]
-			spoken[current_number] = i
-			last_index = spoken[current_number]
-			last_index2 = spoken_before[current_number]
-			if last_index == i && last_index2 == i - 1 {
-				last_number = '1'
-			} else {
-				last_number = ((last_index + 1) - (last_index2 + 1)).str()
-			}
-		}
+	mut position := 0
+	mut positions := []int{len: find}
+	for number in numbers {
+		position += 1
+		positions[number] = position
 	}
-	return last_number.int()
+
+	for position < find {
+		last_position := positions[last_number]
+
+		mut next_number := 0
+		if last_position != 0 {
+			next_number = position - last_position
+		}
+
+		positions[last_number] = position
+		position += 1
+		last_number = next_number
+	}
+
+	return last_number
 }
 
 fn day15a() int {
