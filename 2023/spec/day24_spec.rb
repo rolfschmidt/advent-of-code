7c05e922d3e439f5b0b7327433368715369b8a0c
Rolf Schmidt <rolf@schmidt.ink>
2023-12-26 19:18:49 +0100

    more spaces

--- 2023/spec/day24_spec.rb
+++ 2023/spec/day24_spec.rb
@@ -5,22 +5,25 @@ class Day24 < Helper
 
   def self.elim(m)
     0.upto(m.size-1) do |i|
-        t=m[i][i]; m[i].map!{|x| x/t}
-        (i+1...m.size).each do |j|
-            t=m[j][i]; m[j].map!.with_index{|x,k| x-t*m[i][k]}
+        t = m[i][i]
+        m[i].map!{|x| x / t }
+        (i + 1...m.size).each do |j|
+            t = m[j][i]
+            m[j].map!.with_index{|x,k| x - t * m[i][k] }
         end
     end
     (m.size-1).downto(0) do |i|
-        (0...i).each do |j|
-            t=m[j][i]; m[j].map!.with_index{|x,k| x-t*m[i][k]}
-        end
+      (0...i).each do |j|
+        t=m[j][i]
+        m[j].map!.with_index{|x, k| x - t * m[i][k] }
+      end
     end
     m
   end
 
   def self.mat(stones, x, y, dx, dy)
-    m = stones.map{|s| [-s[dy], s[dx], s[y], -s[x], s[y]*s[dx]-s[x]*s[dy]] }
-    m.take(4).map{|r| r.zip(m[-1]).map{|a,b|(a-b).to_r} }
+    m = stones.map{|s| [ -s[dy], s[dx], s[y], -s[x], s[y] * s[dx] - s[x] * s[dy] ] }
+    m.take(4).map{|r| r.zip(m[-1]).map{|a, b|(a - b).to_r } }
   end
 
   # no math skills, gave up
@@ -33,11 +36,11 @@ class Day24 < Helper
       ax, ay, _, adx, ady, _ = a.map(&:to_f)
       bx, by, _, bdx, bdy, _ = b.map(&:to_f)
 
-      bt = ((bx-ax) / adx - (by-ay) / ady) / (bdy/ady - bdx/adx)
-      at = (bx + bt*bdx - ax) / adx
+      bt = ((bx - ax) / adx - (by - ay) / ady) / (bdy / ady - bdx / adx)
+      at = (bx + bt * bdx - ax) / adx
 
-      ix = ax+adx*at
-      iy = ay+ady*at
+      ix = ax + adx * at
+      iy = ay + ady * at
 
       bt >= 0 && at >= 0 && tmin <= ix && ix <= tmax && tmin <= iy && iy <= tmax
     end
@@ -46,9 +49,9 @@ class Day24 < Helper
   # no math skills, gave up
   # yoinked, props to https://github.com/tckmn/polyaoc-2023/blob/97689dc6b5ff38c557cd885b10be425e14928958/24/rb/24.rb
   def self.part2
-    x, y, *_ = elim(mat stones, 0, 1, 3, 4).map(&:last)
-    z,    *_ = elim(mat stones, 2, 1, 5, 4).map(&:last)
-    return (x+y+z).to_i
+    x, y, *_ = elim(mat(stones, 0, 1, 3, 4)).map(&:last)
+    z,    *_ = elim(mat(stones, 2, 1, 5, 4)).map(&:last)
+    return (x + y + z).to_i
   end
 end
 
