53e506561e146556511bcf8ae509a57377e8c2e6
Rolf Schmidt <rolf@schmidt.ink>
2024-12-01 06:18:47 +0100

    add day 1

---
+++ 2024/spec/day01_spec.rb
@@ -0,0 +1,36 @@
+class Day01 < Helper
+  def self.part1(part2 = false)
+    al = []
+    bl = []
+    file.split("\n").each do |line|
+      data = line.numbers
+      al << data.first
+      bl << data.last
+    end
+
+    al.sort!
+    bl.sort!
+
+    return al.sum do |a|
+      a * bl.count{|x| x == a }
+    end if part2
+
+    return al.map.with_index do |a, ai|
+      [a, bl[ai]].max - [a, bl[ai]].min
+    end.sum
+  end
+
+  def self.part2
+    part1(true)
+  end
+end
+
+RSpec.describe "Day01" do
+  it "does part 1" do
+    expect(Day01.part1).to eq(1258579)
+  end
+
+  it "does part 2" do
+    expect(Day01.part2).to eq(23981443)
+  end
+end
