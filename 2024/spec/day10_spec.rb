7a7c300cd6b4d6bd0e49953d8f637d3db4199c55
Rolf Schmidt <rolf@schmidt.ink>
2024-12-10 07:50:33 +0100

    add day 10

---
+++ 2024/spec/day10_spec.rb
@@ -0,0 +1,51 @@
+class Day10 < Helper
+  def self.trails(map, start, path = [])
+    height = map[start].to_i
+    return [path] if height == 9
+
+    result = []
+    DIRS_PLUS.each do |dir|
+      pos = start + dir
+      next if pos.blank?
+
+      pos_path = path.dup
+
+      next if map[pos].blank?
+      next if map[pos].to_i != height + 1
+      next if pos_path.include?(pos)
+      pos_path << pos
+
+      trails(map, pos, pos_path).each do |found|
+        result << found
+      end
+    end
+    result
+  end
+
+  def self.part1(part2 = false)
+    map = file.to_map.reject_value('.')
+
+    map.dup.reverse['0'].sum do |start, si|
+      result = trails(map, start, [start])
+      if part2
+        result.map(&:last).count
+      else
+        result.map(&:last).uniq.count
+      end
+    end
+  end
+
+  def self.part2
+    part1(true)
+  end
+end
+
+RSpec.describe "Day10" do
+  it "does part 1" do
+    expect(Day10.part1).to eq(512)
+  end
+
+  it "does part 2" do
+    expect(Day10.part2).to eq(1045)
+  end
+end
