019a384601de90b1b73478785d6b6604f6c9b587
Rolf Schmidt <rolf@schmidt.ink>
2024-12-23 12:31:09 +0100

    add 2015 day 24

--- 2015/spec/day24_spec.rb
+++ 2015/spec/day24_spec.rb
@@ -1,30 +1,41 @@
 class Day24 < Helper
-  def self.search(list)
-
-  end
-
-  def self.part1
+  def self.part1(group_count = 3)
     rows = file.lines.map(&:to_i)
 
-    @total = rows.inject(&:*)
-    @total_group = @total / 3
+    @total       = rows.inject(&:+)
+    @total_group = @total / group_count
 
-    pp [@total, @total_group]
+    (1..rows.size).each do |group_size|
+      rows.combination(group_size).each do |group1|
+        next if group1.inject(&:+) % @total_group != 0
 
-    exit
+        rest = rows - group1
+        (1..rest.size).each do |group_size|
+          rows.combination(group_size).each do |group2|
+            next if group2.inject(&:+) % @total_group != 0
+
+            group3 = rest - group2
+            next if group3.inject(&:+) % @total_group != 0
+
+            result = [group1, group2, group3]
+            return result.map { _1.inject(&:*) }.min
+          end
+        end
+      end
+    end
   end
 
   def self.part2
-    100
+    part1(4)
   end
 end
 
 RSpec.describe "Day24" do
   it "does part 1" do
-    expect(Day24.part1).to eq(100)
+    expect(Day24.part1).to eq(11266889531) # 334919618123
   end
 
   it "does part 2" do
-    expect(Day24.part2).to eq(100)
+    expect(Day24.part2).to eq(77387711)
   end
 end
