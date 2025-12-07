54bdfca89d39fdf3f4e02594c193569858e7b47b
Rolf Schmidt <rolf.schmidt@zammad.com>
2025-12-07 09:52:13 +0000

    improvements to day 2

--- 2025/spec/day02_spec.rb
+++ 2025/spec/day02_spec.rb
@@ -1,14 +1,14 @@
 class Day02 < Helper
   def self.part1
-    file.split(',').map(&:strip).sum do |value|
+    Parallel.map(file.split(',')) do |value|
       value.dash_pair.to_range.select do |num|
         num.to_s.halve.same?
       end.sum
-    end
+    end.sum
   end
 
   def self.part2
-    Parallel.map(file.split(',').map(&:strip)) do |value|
+    Parallel.map(file.split(',')) do |value|
       value.dash_pair.to_range.map(&:to_s).select do |chain|
         chain.sequences(count: 1).present?
       end.map(&:to_i).sum
