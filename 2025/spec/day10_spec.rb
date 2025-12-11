04e6305875e37d545d513e22304fee30e0ec7e85
Rolf Schmidt <rolf.schmidt@zammad.com>
2025-12-11 17:59:26 +0000

    2025 day 10 - rename lights to machines and refactor logic

--- 2025/spec/day10_spec.rb
+++ 2025/spec/day10_spec.rb
@@ -1,14 +1,14 @@
 class Day10 < Helper
-  def self.lights
-    @lights ||= begin
-      lights = []
-      file_test.lines.each do |line|
+  def self.machines
+    @machines ||= begin
+      machines = []
+      file.lines.each do |line|
         spaces = line.split(' ')
         light, buttons = spaces[0], spaces[1..]
         light = light[1..-2].chars.map { _1 == '.' ? false : true }
-        lights << [light, buttons[..-2].map(&:numbers), buttons[-1].numbers]
+        machines << [light, buttons[..-2].map(&:numbers), buttons[-1].numbers]
       end
-      lights
+      machines
     end
   end
 
@@ -36,16 +36,16 @@ class Day10 < Helper
       return @best if @best <= 1
     end
 
-    lights[li][1].shuffle.each do |pc|
+    machines[li][1].shuffle.each do |pc|
       next if pressed >= @best
       press(pressed.clone, light.clone, pc.clone, li)
     end
   end
 
   def self.part1
-    Parallel.map(lights.keys) do |li|
-      light = lights[li]
-    # lights.map.with_index do |light, li|
+    Parallel.map(machines.keys) do |li|
+      light = machines[li]
+    # machines.map.with_index do |light, li|
       @best = 10000000000
       @cache = {}
       light[1].each do |pc|
@@ -56,74 +56,38 @@ class Day10 < Helper
     end.sum
   end
 
-  def self.jolly_state(button, jolly)
-    result = jolly.clone
-    pressed = 0
-    while_stable do
-      can_more = true
-      button.each do |value|
-        result[value] -= 1
-        can_more = false if result[value] < 1
-        raise if result[value] < 0
+  # gave up, no math skills
+  # yoinked, props to @firetech, https://github.com/firetech/advent-of-code/blob/0fb1c0178b4b6250487f0a6fbf0191e4331471e4/2025/10/factory_z3.rb
+  def self.part2
+    result = 0
+    machines.each do |_, buttons, joltage|
+      solver = Z3::Optimize.new
+      presses = buttons.map do |bi|
+        z3b = Z3::Int("button_#{bi}")
+        solver.assert(z3b >= 0)
+        z3b
       end
 
-      pressed += 1
+      joltage.each_with_index do |value, counter|
+        jolly_buttons = buttons.keys.select { buttons[_1].include?(counter) }
+        solver.assert(jolly_buttons.map { presses[_1] }.sum == value)
+      end
 
-      # stable if can_more
+      solver.minimize(presses.sum)
+      raise 'z3-error' if !solver.satisfiable?
+
+      result += presses.map { solver.model[_1].to_i }.sum
     end
-    [pressed, result]
-  rescue
-    nil
-  end
-
-  def self.jollize(pressed, buttons, jolly)
-    return if pressed >= @best
-
-    return if @cache[jolly] && @cache[jolly] <= pressed
-    @cache[jolly] = pressed
-
-    if jolly.all?(0) && pressed < @best
-      pp ['best', pressed]
-      @best = pressed
-      return
-    end
-
-    buttons.each do |button|
-      new_pressed, new_jolly = jolly_state(button, jolly)
-      next if new_jolly.blank?
-
-      new_pressed += pressed
-      next if new_pressed >= @best
-
-      jollize(new_pressed, buttons, new_jolly)
-    end
-  end
-
-
-  def self.part2
-    lights.keys.map do |li|
-      data    = lights[li]
-      light   = data[0]
-      buttons = data[1]
-      jolly   = data[2]
-
-      @best = 10000000000
-      @cache = {}
-
-      @cache = {}
-      jollize(0, buttons, jolly)
-
-      @best
-    end.sum
+    result
   end
 end
 
 RSpec.describe "Day10" do
   it "does part 1" do
-    # expect(Day10.part1).to eq(505)
+    expect(Day10.part1).to eq(505)
   end
 
   it "does part 2" do
-    expect(Day10.part2).to eq(100) # 1320000003157
+    expect(Day10.part2).to eq(20002) # 1320000003157
   end
 end
