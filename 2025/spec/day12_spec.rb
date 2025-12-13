077a044112e31a24583beb20922011c891cb3ec0
Rolf Schmidt <rolf.schmidt@zammad.com>
2025-12-13 14:52:06 +0000

    2025 day 12 - optimize shape size calculation and map handling

--- 2025/spec/day12_spec.rb
+++ 2025/spec/day12_spec.rb
@@ -1,6 +1,7 @@
 class Day12 < Helper
   def self.shapes_size(shapes, shape_counts)
-    shape_counts.keys.map { shapes[_1][0].size * shape_counts[_1] }.sum
+    @shapes_size ||= {}
+    @shapes_size[shape_counts] ||= shape_counts.keys.map { shapes[_1][0].size * shape_counts[_1] }.sum
   end
 
   def self.shapes_fit?(map, shapes, shape_counts)
@@ -20,10 +21,14 @@ class Day12 < Helper
             new_shape_counts = shape_counts.clone
             new_shape_counts[shape_index] -= 1
 
-            new_map = map.clone
-            new_map -= shape.map { pos + _1 }
+            # do not clone, instead keep the map
+            # and rollback the map afterwards if no fit
+            # for more speed
+            does_fit = shapes_fit?(map, shapes, new_shape_counts)
+            return true if does_fit
 
-            shapes_fit?(new_map, shapes, new_shape_counts)
+            # rollback map
+            new_map += shape.map { pos + _1 }
           end
         end
       end
