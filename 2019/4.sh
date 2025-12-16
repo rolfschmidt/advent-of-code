01ca27933b37c2cf5dd816b7920f77ba76bc6128
Rolf Schmidt <rolf.schmidt@zammad.com>
2025-12-16 19:34:30 +0000

    remove old

--- 2019/4.sh
+++
@@ -1,7 +0,0 @@
-printf "time 4.js\n" && time node 4.js
-printf "\ntime 4.php\n" && time php 4.php
-printf "\ntime 4.pl\n" && time perl 4.pl
-printf "\ntime 4.py\n" && time python3 4.py
-printf "\ntime 4.rb\n" && time ruby 4.rb
-printf "\ntime 4.rs\n" && time ./4-rs-ubuntu
-printf "\ntime 4.cpp\n" && time ./4-cpp-rs-ubuntu
